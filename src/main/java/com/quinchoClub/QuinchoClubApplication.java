ea88483b2310c9bf4e9d4a448f47c28d5d8d88b8
Pablo Velasco <pablor.velasco.suarez@gmail.com>
2023-11-07 23:14:59 -0300

    primer commit

---
+++ src/main/java/com/quinchoClub/QuinchoClubApplication.java
@@ -0,0 +1,13 @@
+package com.quinchoClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuinchoClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuinchoClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quinchoClub/QuinchoClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinchoClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinchoClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
