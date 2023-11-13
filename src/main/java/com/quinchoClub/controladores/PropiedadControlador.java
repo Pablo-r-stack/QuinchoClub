a8280a601a972a338e933a94e60172ca40d4015b
Pablo Velasco <pablor.velasco.suarez@gmail.com>
2023-11-13 20:23:51 -0300

    se agrego funcionalidad a metodo leer  crear y elimiar entidad Propiedad, vistas html y rutas de thymeleaf

--- src/main/java/com/quinchoClub/controladores/PropiedadControlador.java
+++ src/main/java/com/quinchoClub/controladores/PropiedadControlador.java
@@ -4,8 +4,14 @@
  */
 package com.quinchoClub.controladores;
 
+import com.quinchoClub.entidades.Propiedad;
+import com.quinchoClub.servicios.PropiedadServicio;
 import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +27,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/propiedad")
 public class PropiedadControlador {
+    @Autowired
+    private PropiedadServicio propiedadServicio;
     @GetMapping("/lista")
-    public String listarPropiedades(){
+    public String listarPropiedades(ModelMap modelo){
+        List<Propiedad> propiedades = propiedadServicio.obtenerTodasLasPropiedades();
+        modelo.addAttribute("propiedades", propiedades);
         return "listaPropiedades.html";
     }
     @GetMapping("/registrar")
@@ -31,15 +41,40 @@ public class PropiedadControlador {
     }
     @PostMapping("/registrar")
     public String cargarPropiedad(@RequestParam String tipo, @RequestParam String detalles,
-            @RequestParam String ubicacion, @RequestParam Double tamanio, @RequestParam Date disponibilidad, @RequestParam boolean wifi, @RequestParam boolean pileta, @RequestParam boolean accesorios, @RequestParam boolean cama, @RequestParam boolean aire){
-        return null;
+            @RequestParam String ubicacion, @RequestParam Double tamanio, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date disponibilidad,
+            @RequestParam boolean wifi, @RequestParam boolean pileta, @RequestParam boolean accesorios, @RequestParam boolean cama, @RequestParam boolean aire){
+        Propiedad propiedad = new Propiedad();
+        propiedad.setTipo(tipo);
+        propiedad.setDetalles(detalles);
+        propiedad.setUbicacion(ubicacion);
+        propiedad.setTamanio(tamanio);
+        propiedad.setDisponibilidad(disponibilidad);
+        propiedad.setWifi(wifi);
+        propiedad.setPileta(pileta);
+        propiedad.setAccesorios(accesorios);
+        propiedad.setCama(cama);
+        propiedad.setAire(aire);
+        try {
+        propiedadServicio.crearPropiedad(propiedad);
+            System.out.println("Carga exitosa");
+            return "redirect:/propiedad/lista";
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return "registrarPropiedad.html";
+        }
     }
     @PutMapping("/modificar")
     public String modificarPropiedad(@PathVariable String id){
         return null;
     }
-    @DeleteMapping("/eliminar")
+    @PostMapping("/eliminar/{id}")
     public String eliminarPropiedad(@PathVariable String id){
-        return null;
+        try {
+            propiedadServicio.eliminarPropiedad(id);
+            return "redirect:/propiedad/lista";
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return "redirect:/propiedad/lista";
+        }
     }
 }
