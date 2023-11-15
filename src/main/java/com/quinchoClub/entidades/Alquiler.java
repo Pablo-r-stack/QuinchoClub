0aab029291573410e241cb995665dd7b1b07e089
Ro Pontnau <romapouni@gmail.com>
2023-11-14 21:41:25 -0300

    boton quiero vender ahora lleva al formulario, entidad/service ALQUILER, cambio comentado a Propiedad

---
+++ src/main/java/com/quinchoClub/entidades/Alquiler.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.quinchoClub.entidades;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author rocio
+ */
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Alquiler{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Usuario cliente;
+    @ManyToOne
+    @JoinColumn(name = "propiedad_id")
+    private Propiedad propiedad;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    
+}
