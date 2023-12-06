24a9b85f77689731339acd96a997ea2de2384912
Pablo Velasco <pablor.velasco.suarez@gmail.com>
2023-12-05 21:25:08 -0300

    se agregaron fragments con mensajes de error y exito, se agrego vista error pesonalizada, se corrigieron estilos en post y propiedadesUsuario

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,9 @@
 //MENU BTN
 document.addEventListener('DOMContentLoaded', function () {
-     const menuButton1 = document.getElementById('menuButton1'),
-           menuButton2 = document.getElementById('menuButton2'),
-           menu1 = document.getElementById('menu1'),
-           menu2 = document.getElementById('menu2');
+    const menuButton1 = document.getElementById('menuButton1'),
+            menuButton2 = document.getElementById('menuButton2'),
+            menu1 = document.getElementById('menu1'),
+            menu2 = document.getElementById('menu2');
 
     menuButton1.addEventListener('click', function (event) {
         event.stopPropagation();
@@ -44,72 +44,68 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //CARDS
 const btnLeft = document.querySelector(".btn-left"),
-      btnRight = document.querySelector(".btn-right"),
-      slider = document.querySelector("#slider"),
-      sliderSection = document.querySelectorAll(".slider-section");
+        btnRight = document.querySelector(".btn-right"),
+        slider = document.querySelector("#slider"),
+        sliderSection = document.querySelectorAll(".slider-section");
 
 
 btnLeft.addEventListener("click", e => moveToLeft())
 btnRight.addEventListener("click", e => moveToRight())
 
- setInterval(() => {
-     moveToRight()
- }, 3000);
+setInterval(() => {
+    moveToRight()
+}, 3000);
 
 
 let ope = 0,
-    cont = 0,
-    widthImg = 100 / sliderSection.length;
+        cont = 0,
+        widthImg = 100 / sliderSection.length;
 
 function moveToRight() {
-    if (cont >= sliderSection.length-1) {
+    if (cont >= sliderSection.length - 1) {
         cont = 0;
         ope = 0;
         slider.style.transform = `translate(-${ope}%)`;
         slider.style.transition = "none";
         return;
-    } 
+    }
     cont++;
     ope = ope + widthImg;
     slider.style.transform = `translate(-${ope}%)`;
-    slider.style.transition = "all ease .7s" 
-}  
+    slider.style.transition = "all ease .7s"
+}
 
 function moveToLeft() {
     cont--;
-    if (cont < 0 ) {
-        cont = sliderSection.length-1;
-        ope = widthImg * (sliderSection.length-1)
+    if (cont < 0) {
+        cont = sliderSection.length - 1;
+        ope = widthImg * (sliderSection.length - 1)
         slider.style.transform = `translate(-${ope}%)`;
         slider.style.transition = "none";
         return;
-    } 
+    }
     ope = operacion - widthImg;
     slider.style.transform = `translate(-${ope}%)`;
     slider.style.transition = "all ease .7s"
-} 
+}
 
 //funcion para carteles de error y exitoooooo
-    document.addEventListener('DOMContentLoaded', function () {
-        // Función para mostrar mensajes
-        function mostrarMensaje(tipo, mensaje) {
-            if (tipo === "exito") {
-                alert("¡Éxito! " + mensaje);
-            } else if (tipo === "error") {
-                alert("Error: " + mensaje);
-            }
+function mostrarMensaje(tipo, mensaje) {
+        if (tipo === "exito") {
+            alert("¡Éxito! " + mensaje);
+        } else if (tipo === "error") {
+            alert("Error: " + mensaje);
         }
+    }
 
-        // Verificar si hay mensajes en el DOM y mostrar alertas
-        var mensajes = document.querySelectorAll(".mensaje"); // Ajusta el selector según tu estructura HTML
-        if (mensajes.length > 0) {
-            mensajes.forEach(function (elemento) {
-                var tipo = elemento.getAttribute("data-tipo");
-                var mensaje = elemento.textContent;
-                mostrarMensaje(tipo, mensaje);
-            });
-        }
-    });
-    
+    // Verificar mensajes directamente
+    var mensajeExito = document.getElementById("mExito");
+    var mensajeError = document.getElementById("mError");
+
+    if (mensajeExito && mensajeExito.textContent.trim() !== "") {
+        mostrarMensaje("exito", mensajeExito.textContent);
+    } else if (mensajeError && mensajeError.textContent.trim() !== "") {
+        mostrarMensaje("error", mensajeError.textContent);
+    }
 // FALTA FUNCION PARA MANIPULAR LAS STARS DE LAS CARDS
 
